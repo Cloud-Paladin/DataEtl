3faee84fe180e65aafe20ecf13bf370ad8dea790
libing <853666993@qq.com>
2019-01-28 17:34:04 +0800

    zookeeper constant

--- common/src/main/java/com/dataexp/common/zookeeper/example/part2/TestWatch.java
+++ common/src/main/java/com/dataexp/common/zookeeper/example/part2/TestWatch.java
@@ -14,7 +14,9 @@ import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 
 public class TestWatch implements Watcher, Runnable {
-    private static String hostPort = "localhost:2181";
+    private static final String hostPort = "localhost:2181";
+    private static final String chroot = "/datadistclean";
+    //    private static String hostPort = "localhost:2181/datadistclean";
     //    private static String hostPort = "localhost:2181/WatcherTest";
     private static String zooDataPath = "/MyConfig";
     private CountDownLatch startLock = new CountDownLatch(1);
@@ -29,11 +31,22 @@ public class TestWatch implements Watcher, Runnable {
             startLock.await();
             if (zk != null) {
                 try {
+                    /**
+                     * 如果chroot地址不存在，则初始化该节点
+                     */
+                    if (null == zk.exists(chroot, false)) {
+                        zk.create(chroot, "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+                        zk.close();
+                    }
+                    //重新以chroot为根路径连接到Zookeeper
+                    startLock = new CountDownLatch(1);
+                    zk = new ZooKeeper(hostPort + chroot, 2000, this);
+                    startLock.await();
                     //Create the znode if it doesn't exist, with the following code:
                     if (zk.exists(zooDataPath, null) == null) {
                         zk.create(zooDataPath, "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
                     }
-
+                    zk.getChildren(zooDataPath, true);
                 } catch (KeeperException | InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -55,18 +68,18 @@ public class TestWatch implements Watcher, Runnable {
     @Override
     public void process(WatchedEvent event) {
         System.out.printf("Event Received: %s\n", event.toString());
-        System.out.println(count.incrementAndGet());
+        System.out.println("count:" + count.incrementAndGet());
         //We will process only events of type NodeDataChanged
         if (event.getState() == Event.KeeperState.SyncConnected && event.getType() == Event.EventType.None) {
             startLock.countDown();
-            return;
-        }
-        try {
-            printData();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (KeeperException e) {
-            e.printStackTrace();
+        } else if (event.getState() == Event.KeeperState.SyncConnected && event.getType() == Event.EventType.NodeChildrenChanged) {
+            try {
+                printData();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (KeeperException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -88,11 +101,9 @@ public class TestWatch implements Watcher, Runnable {
             throws InterruptedException, KeeperException {
         TestWatch testWatcher = new TestWatch();
         new Thread(testWatcher).start();
-        testWatcher.zk.getChildren(zooDataPath,testWatcher);
         testWatcher.zk.create(zooDataPath + "/child1", "first".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-        testWatcher.zk.create(zooDataPath + "/child11", "first".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-        testWatcher.zk.create(zooDataPath + "/child12", "first".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-
+        testWatcher.zk.create(zooDataPath + "/child2", "first".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        testWatcher.zk.create(zooDataPath + "/child3", "first".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
 //        for (int i = 0; i < 100; i++) {
 //            testWatcher.zk.create(zooDataPath + "/child"+i, "first".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
 //        }
